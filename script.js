d62430cef6b6049120ee0ee4cad99f9dd8b8b514
Federico Gentili <federicogentili05@gmail.com>
2026-01-12 21:19:05 +0100

    refactor

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const password = "Epicode123";
 let userTry = 0;
 
 do {
+  userTry++;
   let prova = prompt(`Prova ad indovinare la password! (${userTry} - 3)`);
   if (prova === password) {
     break;
   }
-  userTry++;
 } while (userTry < 3);
 
-if (userTry < 3) alert("Hai vinto!");
+if (userTry < 3)
+  alert(
+    `Hai vinto! Ci sei riuscito in ${userTry} ${
+      userTry === 1 ? "tentativo" : "tentativi"
+    }`
+  );
 else alert("Hai perso.");
